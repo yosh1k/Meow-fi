b4f3cb1c09c504631051849c8b0994a408e6ccaa
yosh1k <128407300+yosh1k@users.noreply.github.com>
2023-03-27 16:20:35 +0300

    Update deal_controller.go

--- internal/services/usercase/controller/deal_controller.go
+++ internal/services/usercase/controller/deal_controller.go
@@ -1,9 +1,30 @@
 package controller
 
 import (
+	"Meow-fi/internal/models"
 	"Meow-fi/internal/services/usercase/repo"
 )
 
 type DealInteractor struct {
 	DealRepository repo.DealRepository
+}
+
+func (interactor *DealInteractor) Add(t models.Deal) {
+	interactor.DealRepository.Store(t)
+}
+func (interactor *DealInteractor) UpdateDeal(t models.Deal) {
+	interactor.DealRepository.UpdateDeal(t)
+}
+func (interactor *DealInteractor) GetAllDeal() []models.Deal {
+	return interactor.DealRepository.Select()
+}
+func (interactor *DealInteractor) GetDeal(id string) models.Deal {
+	return interactor.NoticeRepository.SelectById(id)
+}
+func (interactor *DealInteractor) GetNoticeInfo(id string) models.Deal {
+	deal := interactor.DealRepository.GetDealInfo(id)
+	return deal
+}
+func (interactor *DealInteractor) Delete(id string) {
+	interactor.DealRepository.Delete(id)
 }
