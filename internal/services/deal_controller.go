10818f3c3b209041393c197ebcb8ca1148d45aa1
yosh1k <128407300+yosh1k@users.noreply.github.com>
2023-03-27 16:22:12 +0300

    Update deal_controller.go

--- internal/services/deal_controller.go
+++ internal/services/deal_controller.go
@@ -1,7 +1,60 @@
 package services
+	
 
-import "Meow-fi/internal/services/usercase/controller"
+	import (
+		"Meow-fi/internal/database"
+		"Meow-fi/internal/database/interfaces"
+		"Meow-fi/internal/models"
+		"Meow-fi/internal/services/usercase/controller"
+	
 
-type DealController struct {
-	Interactor controller.DealInteractor
-}
+		"github.com/labstack/echo"
+	)
+	
+
+	type DealController struct {
+		Interactor controller.DealInteractor
+	}
+	
+
+	func NewDealController(sqlHandler interfaces.SqlHandler) *DealController {
+		return &DealController{
+			Interactor: controller.DealInteractor{
+				DealRepository: &database.DealRepository{
+					SqlHandler: sqlHandler,
+				},
+			},
+		}
+	}
+	
+
+	func (controller *DealController) Create(ctx echo.Context) {
+		Deal := models.Deal{}
+		ctx.Bind(&Deal)
+		controller.Interactor.Add(Deal)
+		createdDeals := controller.Interactor.GetAllDeals()
+		ctx.JSON(201, createdDeals)
+		return
+	}
+	func (controller *DealController) UpdateDeal(t models.Deal) {
+	
+
+	}
+	func (controller *DealController) GetDeal(id string) models.Deal {
+		Deal := controller.Interactor.GetDeal(id)
+		return Deal
+	}
+	func (controller *DealController) GetDealInfo(id string) string {
+		Deal := controller.Interactor.GetDealInfo(id)
+		str := ""
+		str += Deal.Client.FIO + " created Deal: " + Deal.Containing
+		return str
+	}
+	func (controller *DealController) Delete(id string) {
+		controller.Interactor.Delete(id)
+	}
+	func (controller *DealController) GetAllDeals() []models.Deal {
+		res := controller.Interactor.GetAllDeals()
+		return res
+	}
+
